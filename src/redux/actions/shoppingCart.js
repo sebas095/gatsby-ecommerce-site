65a0867f7c76bd31f0cb09595406bf5b3f68ed42
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2020-06-05 00:37:11 -0500

    [ADD] actions added

---
+++ src/redux/actions/shoppingCart.js
@@ -0,0 +1,18 @@
+import { ADD_ITEM, CLEAR_SHOPPING_CART } from "../actionTypes"
+
+//Action creators for shopping_cart
+// Funciones que retornan acciones
+
+/**
+ * { type: '', payload: {} }
+ *
+ */
+
+export const addItem = product => ({
+  type: ADD_ITEM,
+  payload: product,
+})
+
+export const clearShoppingCart = () => ({
+  type: CLEAR_SHOPPING_CART,
+})
