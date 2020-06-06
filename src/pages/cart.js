44642f5607185a44f8edf3eb6926c1248d6bb5dd
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2020-06-05 22:04:11 -0500

    [ADD] shoppingcart page added

--- src/pages/cart.js
+++ src/pages/cart.js
@@ -1,11 +1,11 @@
 import React from "react"
-import { useSelector } from "react-redux"
+import { useSelector, shallowEqual } from "react-redux"
 
 import Layout from "../components/Layout"
 import ShoppingCart from "../components/ShoppingCart"
 
 export default () => {
-  const shoppingCart = useSelector(state => state.shoppingCart)
+  const shoppingCart = useSelector(state => state.shoppingCart, shallowEqual)
 
   return (
     <Layout>
