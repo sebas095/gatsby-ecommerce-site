5ab04eb7c134c88027ec090bb12553e1d0675b54
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2020-06-05 23:37:34 -0500

    [ADD] logo added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,12 @@
 import React from "react"
 
 import Layout from "../components/Layout"
+import logo from "../img/ecommerce-logo.png"
 
 export default () => (
   <Layout>
-    <h1>Hola Mundo</h1>
+    <div className="text-center overflow-hidden">
+      <img className="h-80 inline-block" src={logo} alt="logo" />
+    </div>
   </Layout>
 )
